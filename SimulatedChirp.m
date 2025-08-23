27962a771e2e137a4948deed66c949b40a81a2e3
losterberg3 <losterberg@ucsd.edu>
2025-08-23 16:09:41 -0700

    w

--- SimulatedChirp.m
+++ SimulatedChirp.m
@@ -9,26 +9,60 @@ q=read(s,41377,'uint8');  %read the data, 43194 samples for a sampling duration 
 dc=127; %DC offset
 p=q-dc; %using the DC offset to bring the samples around 0 and then to normalize the samples
 p=p./dc; %originally the DC offset was 507, but we had to convert to an 8 bit number to maximize the serial baud rate
-sr=41377; %sampling rate
 
+%% simulation using cross-correlation theory
+sr = 50000;
 cl=0.5; %chirplength in seconds
-sf=1000; %start frequency of the chirp
-ef=1000; %end frequency of the chirp
+sf=500; %start frequency of the chirp
+ef=10000; %end frequency of the chirp
 t=0:1/(sr-1):1; %time we are listening for the chirp
 f=(t.^2*(ef-sf)/cl/2)+(t.*sf); %frequency linearly increasing (frequency is the derivative of this wrt t)
 ref=sin(f.*2*pi); %reference chirp signal
 ref(1001:sr)=0; %setting the rest of the reference signal to 0, because our t is a total of 1 sec, but our chirp is only 0.5 sec
 sig=zeros(1,sr);
-sig(:,101:1100)=-p(:,1:1000); %setting the negative of the samples gives better results, it seems like the microphone receives inverted of the reference
-X=fft(ref); %fft of the reference signal
-Y=fft(sig); %fft of the sampled signal
-Z=Y.*conj(X); %multiplying them in frequency domain
-z=ifft(Z); %ifft of the result to get back to the time domain
-[m,i]=max(z); %finding the index of the peak
-tof=(i-100)*(1/(sr-1)); %converting that index to a time
-d=tof*343; %converting the tof to a distance by multiplying by the speed of sound
-plot(z(1,1:1000))
 
+% Define a delay in seconds
+delta_t = 0.01;  % 10 ms delay
+delta_samples = round(delta_t * sr);
+
+% Shift ref by delta_samples and add noise
+sig = zeros(size(ref));
+if delta_samples < length(ref)
+    sig(delta_samples+1:end) = ref(1:end-delta_samples);
+end
+
+% Add Gaussian noise
+noise_level = 0.05;  % adjust amplitude of noise
+sig = sig + noise_level * randn(size(sig));
+
+% Now you can continue with your cross-correlation
+X = fft(ref);
+Y = fft(sig);
+Z = Y .* conj(X);
+z = ifft(Z);
+
+% Time vector for correlation (same as t)
+t_corr = t;
+
+% Plot everything together
+figure;
+subplot(3,1,1)
+plot(t(1:4000), ref(1:4000))
+xlabel('Time (s)')
+ylabel('Amplitude')
+title('Reference Chirp')
+
+subplot(3,1,2)
+plot(t(1:4000), sig(1:4000))
+xlabel('Time (s)')
+ylabel('Amplitude')
+title('Delayed & Noisy Signal')
+
+subplot(3,1,3)
+plot(t_corr(1:4000), abs(z(1:4000)))  % absolute value for clarity
+xlabel('Time (s)')
+ylabel('Correlation')
+title('Cross-correlation Result')
 %% 
 
 tt=0:1/(sr-1):199/(sr-1);
